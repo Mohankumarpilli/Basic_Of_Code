286b3f7de34bad3b2d70f419cd1dcc02b99d1481
Mohankumarpilli <mohankumarpilli15@gmail.com>
2024-11-02 12:35:25 +0530

    edited some

--- Solution.java
+++ Solution.java
@@ -23,6 +23,5 @@ class Solution {
 
     public static void main(String[] args) {
         System.out.println(postToInfix("ab+"));
-        System.out.println("Hello");
     }
 }
--- Wapper.java
+++
@@ -1,8 +0,0 @@
-public class Wapper {
-    public static void main(String[] args) {
-          int x=34;
-          float y=3.23f;
-          long z=654;
-
-    }
-}
